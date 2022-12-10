3d3fc20ea2ad7246f77c82aa62be3e99e4bbd7c0
Briella94 <nathaliefijan@msn.com>
2022-12-10 20:01:50 +0100

    Succesvolle filtering op inspectienummer

--- src/app/inspection-details/inspection-details.component.ts
+++ src/app/inspection-details/inspection-details.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-inspection-details',
@@ -8,15 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class InspectionDetailsComponent implements OnInit {
+  public providedId: number = 0;
   public inspectionData: any;
-  public constructor(private http: HttpClient) {}
+  public constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     const url: string = 'https://my-json-server.typicode.com/Briella94/LOI/db';
     this.http.get(url).subscribe((response) => {
       this.inspectionData = response;
     })
-  }
 
-  displayedColumns: string[] = ['detailsKeys', 'detailsValues'];
+    this.route.queryParams
+    .subscribe(params => {
+      let clickedId = params['clickedId'];
+      this.providedId = clickedId;
+      console.log(clickedId);
+    })
+  };
+
 }
