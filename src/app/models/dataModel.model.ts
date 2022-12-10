7bae8324724374ac24ae18afebebe7ece9bfd66b
Briella94 <nathaliefijan@msn.com>
2022-12-10 16:41:30 +0100

    Uitgangspunt voor de avond

--- src/app/models/dataModel.model.ts
+++ src/app/models/dataModel.model.ts
@@ -1,35 +1,41 @@
-export interface IInspection {
-    inspectionId: number;
-    inspectionLocation: string;
-    inspectionDate: string;
-    damages: Array<{
+export class Inspection {
+    inspections: Array<{
+      inspectionId: number;
+      inspectionLocation: string;
+      inspectionDate: string;
+      damages: Array<{
         damageType: string;
         damageLocation: string;
         damageNew: string;
         damageDate: string;
         damageActionRequired: string;
         damageDescription: string;
-    }>
-    deferrences: Array<{
+      }>
+      deferrences: Array<{
         deferrenceType: string;
         deferrenceLocation: string;
         deferrenceActionRequired: string;
         deferrenceCostIndication: string;
-    }>
-    installations: Array<{
+      }>
+      installations: Array<{
         installationType: string;
         installationLocation: string;
         installationReports: string;
         installationTestingProcedure: string;
         installationApproved: string;
         installationRemark: string;
-    }>
-    modifications: Array<{
+      }>
+      modifications: Array<{
         modificationDescription: string;
         modificationLocation: string;
         modificationDocumentation: string;
         modificationBy: string;
         modificationActionRequired: string;
         modificationRemark: string;
-    }>
-}
+      }>
+    }>;
+  
+    constructor(inspections: any) {
+      this.inspections = inspections;
+    }
+  }
