24cc9c03a728a7912bafb1d1035636852643e125
Briella94 <nathaliefijan@msn.com>
2022-12-10 16:40:00 +0100

    Nog een attempt om te restoren...

--- src/app/inspection-details/inspection-details.component.ts
+++ src/app/inspection-details/inspection-details.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-inspection-details',
@@ -10,20 +9,14 @@ import { ActivatedRoute } from '@angular/router';
 
 export class InspectionDetailsComponent implements OnInit {
   public inspectionData: any;
-  providedId: number = 0;
-  public constructor(private http: HttpClient, private route: ActivatedRoute) {}
+  public constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     const url: string = 'https://my-json-server.typicode.com/Briella94/LOI/db';
     this.http.get(url).subscribe((response) => {
       this.inspectionData = response;
     })
-  
-    this.route.queryParams
-      .subscribe(params => {
-        this.providedId = params['clickedId'];
-        console.log(this.providedId);
-    })
-}
+  }
 
+  displayedColumns: string[] = ['detailsKeys', 'detailsValues'];
 }
