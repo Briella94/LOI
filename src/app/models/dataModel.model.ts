bfdd6bffb2b4d301604d102c3167489b9bfde2ef
Briella94 <nathaliefijan@msn.com>
2022-12-11 11:16:33 +0100

    Update dataModel.model.ts

--- src/app/models/dataModel.model.ts
+++ src/app/models/dataModel.model.ts
@@ -1,42 +1,9 @@
 export class Inspection {
-    inspections: Array<{
       inspectionId: number;
       inspectionLocation: string;
-      inspectionDate: string;
-      inspectionCompleted: boolean;
-      damages: Array<{
-        damageType: string;
-        damageLocation: string;
-        damageNew: string;
-        damageDate: string;
-        damageActionRequired: string;
-        damageDescription: string;
-      }>
-      deferrences: Array<{
-        deferrenceType: string;
-        deferrenceLocation: string;
-        deferrenceActionRequired: string;
-        deferrenceCostIndication: string;
-      }>
-      installations: Array<{
-        installationType: string;
-        installationLocation: string;
-        installationReports: string;
-        installationTestingProcedure: string;
-        installationApproved: string;
-        installationRemark: string;
-      }>
-      modifications: Array<{
-        modificationDescription: string;
-        modificationLocation: string;
-        modificationDocumentation: string;
-        modificationBy: string;
-        modificationActionRequired: string;
-        modificationRemark: string;
-      }>
-    }>;
-  
-    constructor(inspections: any) {
-      this.inspections = inspections;
+
+    constructor(inspectionId: number, inspectionLocation: string) {
+      this.inspectionId = inspectionId;
+      this.inspectionLocation = inspectionLocation;
     }
   }
